21dd80160dcbf443e311e133b4ca344223a8399c
jhuntoo <jonnolee84@gmai.com>
2017-04-19 16:27:28 +0100

    rename Record to ParsedIp

--- src/parse.ts
+++ src/parse.ts
@@ -1,11 +1,14 @@
-export class Record {
-   constructor(public ip: string) {
+import * as validator from 'validator';
 
-   }
+export class ParsedIP {
+    constructor(public value: string,
+                public isValid: boolean) {
+
+    }
 }
 
-export function parse(line: string): Record {
-    const fields = line.split('\t');
-    const ip = fields[23];
-    return new Record(ip);
+export function parse(line: string): ParsedIP {
+        const fields = line.split('\t');
+        const ip = fields[23];
+        return new ParsedIP(ip, validator.isIP(ip))
 }
